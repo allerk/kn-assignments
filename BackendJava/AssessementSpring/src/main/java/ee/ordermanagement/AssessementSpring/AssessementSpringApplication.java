bb99158602a84c06aac5607502d176d9869a08bd
Aleksandr Lerko <allerk@ttu.ee>
2023-09-16 21:14:14 +0300

    feat: add main file

---
+++ BackendJava/AssessementSpring/src/main/java/ee/ordermanagement/AssessementSpring/AssessementSpringApplication.java
@@ -0,0 +1,13 @@
+package ee.ordermanagement.AssessementSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessementSpringApplication.class, args);
+	}
+
+}
